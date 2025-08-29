9660f4ba8f90884c7e4de0306ea2c6d1f77b0721
kabish <kabishpradhan84@gmail.com>
2025-08-29 12:38:48 +0545

    small changes

--- auctionhub-Backend/auctionhub/settings.py
+++ auctionhub-Backend/auctionhub/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:5173",
+    "http://localhost:5174",
 ]
 
 ROOT_URLCONF = 'auctionhub.urls'
