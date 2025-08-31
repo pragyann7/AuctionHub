957634fd005465e921e80ab4123243d184c2a1a2
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-08-31 23:55:11 +0545

    Created a AuthContext object to handle/share state using useContext hook

---
+++ auctionhub-Frontend/context/AuthContext.jsx
@@ -0,0 +1,86 @@
+import React, { useContext, useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../API/axiosInstance";
+
+const AuthContext = createContext();
+export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem('access');
+        if (token) {
+            fetchUserData();
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+
+    const fetchUserData = async () => {
+        try {
+            const res = await axiosInstance.get('/users/me/');
+            setUser(res.data);
+            setIsAuthenticated(true);
+        } catch (error) {
+            console.log(error);
+            logout();
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const login = async (credentials) => {
+        try {
+            const res = await axiosInstance.post('/login/', credentials);
+            localStorage.setItem('access', res.data.access);
+            localStorage.setItem('refresh', res.data.refresh);
+            alert('Login Successful!');
+            await fetchUserData();
+            navigate('/');
+        } catch (error) {
+            console.error('Login failed', error);
+            alert('Login failed', error);
+            return
+        }
+    }
+
+    const logout = async () => {
+        const confirmed = window.confirm("Do you want to log out?");
+        if (!confirmed) return;
+
+        const refresh = localStorage.getItem('refresh');
+
+        if (!refresh) {
+            localStorage.clear();
+            navigate('/');
+            return;
+        }
+
+        try {
+            await axiosInstance.post('logout/', { refresh });
+            console.log('Logout Successful');
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data.error);
+            } else {
+                console.log(error.message);
+            }
+        } finally {
+            setUser(null);
+            setIsAuthenticated(false);
+            localStorage.clear();
+            navigate('/');
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, isAuthenticated, login, logout, loading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
