2f53fc0b0d262a1abca8f9796bf1170e4d00782b
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-01 15:31:58 +0545

    Created: Protected Route component

---
+++ auctionhub-Frontend/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated, loading } = useContext(AuthContext);
+
+    if (loading) return <div className="flex justify-center items-center h-screen">Loading...</div>;
+
+    return isAuthenticated ? children : <Navigate to="/signin" />;
+};
+
+export default ProtectedRoute;
