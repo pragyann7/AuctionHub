c8b7eb729ffdf91de70d17127142d95bbf23c5d4
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-03 21:03:21 +0545

    Added: New AddProduct route

--- auctionhub-Frontend/src/App.jsx
+++ auctionhub-Frontend/src/App.jsx
@@ -2,18 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from '../components/SignUp'
 import SignIn from '../components/SignIn'
 import Home from '../components/Home'
+import AddProduct from '../components/AddProduct';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import Layout from '../Layout/Layout';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+
+          <Route path="/addproduct" element={<AddProduct />} />
+        </Routes>
+      </Layout>
     </>
   )
 }
