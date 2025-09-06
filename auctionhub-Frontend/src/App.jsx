28eea56687e709fd8462efc84516d2c782ffea37
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-06 21:41:34 +0545

    main routes wrapped around protected route

--- auctionhub-Frontend/src/App.jsx
+++ auctionhub-Frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from '../components/SignUp'
 import SignIn from '../components/SignIn'
 import Home from '../components/Home'
 import AddProduct from '../components/AddProduct';
 import UserProfile from '../components/UserProfile';
 import ContactUs from '../components/ContactUs'
+import Browse from '../components/Browse';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import Layout from '../Layout/Layout';
 import './App.css'
@@ -14,12 +15,13 @@ function App() {
     return (
         <Layout>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/signin" element={<SignIn/>}/>
-                <Route path="/userprofile" element={<UserProfile/>}/>
-                <Route path="/addproduct" element={<AddProduct/>}/>
-                <Route path="/contact" element={<ContactUs/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/userprofile" element={<ProtectedRoute> <UserProfile /> </ProtectedRoute>} />
+                <Route path="/addproduct" element={<ProtectedRoute> <AddProduct /> </ProtectedRoute>} />
+                <Route path="/contact" element={<ProtectedRoute> <ContactUs /> </ProtectedRoute>} />
+                <Route path="/Browse" element={<ProtectedRoute> <Browse /> </ProtectedRoute>} />
             </Routes>
         </Layout>
     )
