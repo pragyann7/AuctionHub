795a634a621cd194cfaa238a490c05289aa7349d
kabish <kabishpradhan84@gmail.com>
2025-09-14 21:10:19 +0545

    new route added

--- auctionhub-Frontend/src/App.jsx
+++ auctionhub-Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import AddProduct from '../components/AddProduct';
 import UserProfile from '../components/UserProfile';
 import {ContactUs} from '../components/ContactUs'
 import Browse from '../components/Browse';
+import EditProfile from '../components/EditProfile'
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import Layout from '../Layout/Layout';
 import './App.css'
@@ -19,6 +20,7 @@ function App() {
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
                 <Route path="/userprofile" element={<ProtectedRoute> <UserProfile /> </ProtectedRoute>} />
+                <Route path="/editprofile" element={<ProtectedRoute><EditProfile /> </ProtectedRoute>} />
                 <Route path="/addproduct" element={<ProtectedRoute> <AddProduct /> </ProtectedRoute>} />
                 <Route path="/contact" element={<ProtectedRoute> <ContactUs /> </ProtectedRoute>} />
                 <Route path="/Browse" element={<ProtectedRoute> <Browse /> </ProtectedRoute>} />
