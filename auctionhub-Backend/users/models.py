b2675d5cd9b725370797849f85332d4d62d6e59d
kabish <kabishpradhan84@gmail.com>
2025-09-14 20:45:51 +0545

    new global function added

--- auctionhub-Backend/users/models.py
+++ auctionhub-Backend/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -98,3 +100,11 @@ class Bid(models.Model):
         # Update product current_bid automatically
         self.product.current_bid = self.amount
         self.product.save()
+
+@receiver(post_save,sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        if hasattr(instance,'profile'):
+            instance.profile.save()
