10a172e302babbcc13edd8bfb4a7f4bbf150e0ac
kabish <kabishpradhan84@gmail.com>
2025-09-15 20:43:27 +0545

    minor changes to fixed navigate issue

--- auctionhub-Frontend/src/App.jsx
+++ auctionhub-Frontend/src/App.jsx
@@ -1,32 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignUp from '../components/SignUp'
-import SignIn from '../components/SignIn'
-import Home from '../components/Home'
+import ScrollToTop from '../utils/ScrollToTop';
+import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
+import Home from '../components/Home';
 import AddProduct from '../components/AddProduct';
 import UserProfile from '../components/UserProfile';
-import {ContactUs} from '../components/ContactUs'
+import { ContactUs } from '../components/ContactUs';
 import Browse from '../components/Browse';
-import EditProfile from '../components/EditProfile'
+import EditProfile from '../components/EditProfile';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-import Layout from '../Layout/Layout';
-import './App.css'
+import Layout from '../Utils/Layout.jsx';
+import './App.css';
 
 function App() {
-
     return (
         <Layout>
+            <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
-                <Route path="/userprofile" element={<ProtectedRoute> <UserProfile /> </ProtectedRoute>} />
-                <Route path="/editprofile" element={<ProtectedRoute><EditProfile /> </ProtectedRoute>} />
-                <Route path="/addproduct" element={<ProtectedRoute> <AddProduct /> </ProtectedRoute>} />
-                <Route path="/contact" element={<ProtectedRoute> <ContactUs /> </ProtectedRoute>} />
-                <Route path="/Browse" element={<ProtectedRoute> <Browse /> </ProtectedRoute>} />
+                <Route path="/userprofile" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
+                <Route path="/editprofile" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+                <Route path="/addproduct" element={<ProtectedRoute><AddProduct /></ProtectedRoute>} />
+                <Route path="/contact" element={<ProtectedRoute><ContactUs /></ProtectedRoute>} />
+                <Route path="/Browse" element={<ProtectedRoute><Browse /></ProtectedRoute>} />
             </Routes>
         </Layout>
-    )
+    );
 }
 
-export default App
+export default App;
