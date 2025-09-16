869d7037640f68da205ae06a68f350f7cfd0d788
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-16 11:04:07 +0545

    added views for CRUD

--- auctionhub-Backend/products/views.py
+++ auctionhub-Backend/products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import AuctionProduct, AuctionProductImage
+from .serializers import AuctionProductSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+
+class AuctionProductListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = AuctionProduct.objects.all()
+    serializer_class = AuctionProductSerializer
+
+    def perform_create(self, serializer):
+        product = serializer.save()
+        images = self.request.FILES.getlist('images')
+        for image in images:
+            AuctionProductImage.objects.create(product=product, image=image)
+
+
+class AuctionProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = AuctionProduct.objects.all()
+    serializer_class = AuctionProductSerializer
