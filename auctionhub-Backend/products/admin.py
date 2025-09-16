5ed5f24eff1d3388fc4e8a9fa64ea8473ab658dd
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-16 11:01:59 +0545

    added fields to view product details in admin django

--- auctionhub-Backend/products/admin.py
+++ auctionhub-Backend/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AuctionProduct
 
-# Register your models here.
+@admin.register(AuctionProduct)
+class AuctionProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'auction_start_datetime', 'auction_duration', 'starting_price')
