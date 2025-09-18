dc3696b2cd02327b9c89b6c2c4dc47a0aafd6b31
kabish <kabishpradhan84@gmail.com>
2025-09-18 15:40:38 +0545

    websockets setup

--- auctionhub-Backend/auctionhub/asgi.py
+++ auctionhub-Backend/auctionhub/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import auctions.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auctionhub.settings')
+django.setup()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            [
+                auctions.routing.websocket_urlpatterns,
+            ]
+        )
+    )
+})
