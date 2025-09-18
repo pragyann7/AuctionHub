2f5d050aa72692b64ad452b268abda3fc42fdb83
kabish <kabishpradhan84@gmail.com>
2025-09-18 19:58:24 +0545

    minor warning fixed

--- auctionhub-Backend/auctionhub/asgi.py
+++ auctionhub-Backend/auctionhub/asgi.py
@@ -12,18 +12,17 @@ import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import auctions.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auctionhub.settings')
 django.setup()
+
+import auctions.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            [
-                auctions.routing.websocket_urlpatterns,
-            ]
+                auctions.routing.websocket_urlpatterns
         )
     )
 })
