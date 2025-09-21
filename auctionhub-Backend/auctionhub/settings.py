2f1b544a6ca41861d084340f49a6f992f0e60388
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-21 23:41:37 +0545

    OTP sending  mail settings added, legit gmail account

--- auctionhub-Backend/auctionhub/settings.py
+++ auctionhub-Backend/auctionhub/settings.py
@@ -183,3 +183,14 @@ CACHES = {
 # Optional: store sessions in Redis
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'theeauctionhub@gmail.com'
+EMAIL_HOST_PASSWORD = 'ufhb kqlt ehru tnws'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = 'no-reply@auctionhub.com'
