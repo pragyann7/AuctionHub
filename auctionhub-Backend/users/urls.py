7fa20ee6910faed8b5c3c743936efb3ed282eeda
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-21 23:42:35 +0545

    new url added verifyEmail and resendOTP also modified old url

--- auctionhub-Backend/users/urls.py
+++ auctionhub-Backend/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import RegisterView, CheckAvailabilityView, UserFetchView, LogoutView, \
-    EditProfileView, BecomeSellerView, PublicUserDetailAPIView
+    EditProfileView, BecomeSellerView, PublicUserDetailAPIView,VerifyEmailOTPView, ResendOTPView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.tokens import MyTokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('users/me/', UserFetchView.as_view(), name='user-fetch'),
@@ -13,5 +15,7 @@ urlpatterns = [
     path("check-availability/", CheckAvailabilityView.as_view(), name="check-availability"),
     path('edit-profile/', EditProfileView.as_view(), name='edit-profile'),
     path('become-seller/', BecomeSellerView.as_view(), name='become-seller'),
+    path('verify-email/', VerifyEmailOTPView.as_view(), name='verify-email'),
+    path('resend-otp/', ResendOTPView.as_view(), name='resend-otp'),
 
 ]
