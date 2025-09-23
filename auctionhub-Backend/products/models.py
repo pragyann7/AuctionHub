21822cd60a81b1650d103d1f507d3e265fe52a1f
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-23 22:40:49 +0545

    new wishlist model added

--- auctionhub-Backend/products/models.py
+++ auctionhub-Backend/products/models.py
@@ -89,3 +89,16 @@ class AuctionProduct(models.Model):
 class AuctionProductImage(models.Model):
     product = models.ForeignKey(AuctionProduct, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/')
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wishlist")
+    product = models.ForeignKey("AuctionProduct", on_delete=models.CASCADE, related_name="wishlisted_by")
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "product")
+        ordering = ["-added_at"]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
