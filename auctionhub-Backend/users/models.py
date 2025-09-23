ef25e17ca1964c1f1015c7419cad9e7492a724a7
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-23 22:42:59 +0545

    added new follow model

--- auctionhub-Backend/users/models.py
+++ auctionhub-Backend/users/models.py
@@ -61,3 +61,12 @@ class EmailOTP(models.Model):
 
     def __str__(self):
         return f"{self.user.email} – {self.code}"
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
